20d7cecbb2ba476ed7ede179378a8baa5fee8648
student <student@240-06.INFORMATYKA.local>
2016-10-06 11:02:12 +0200

    zad2

--- src/org/yourorghere/Projekt1.java
+++ src/org/yourorghere/Projekt1.java
@@ -89,14 +89,19 @@ public class Projekt1 implements GLEventListener {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         // Reset the current matrix to the "identity"
         gl.glLoadIdentity();
-
+        gl.glTranslatef(-1.5f,0.0f,-6.0f);
        gl.glBegin(GL.GL_TRIANGLES);
-gl.glVertex3f(-1.0f, 1.0f, -6.0f);
-gl.glVertex3f(-2.0f,-1.0f, -6.0f);
-gl.glVertex3f( 0.0f,-1.0f, -6.0f);
-gl.glEnd();
+            gl.glVertex3f(-1.0f, 1.0f, -6.0f);
+            gl.glVertex3f(-2.0f,-1.0f, -6.0f);
+            gl.glVertex3f( 0.0f,-1.0f, -6.0f);
+       gl.glEnd();
 
-
+        gl.glBegin(GL.GL_QUADS);
+            gl.glVertex3f( 2.0f, 3.0f, -6.0f);
+            gl.glVertex3f(2.0f, -2.0f, -6.0f);
+            gl.glVertex3f( 4.0f, -2.0f, -6.0f);
+            gl.glVertex3f( 4.0f, 3.0f, -6.0f);
+       gl.glEnd();
         // Flush all drawing operations to the graphics card
         gl.glFlush();
     }
